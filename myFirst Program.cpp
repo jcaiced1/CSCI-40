d12a6e7212f964b728f40a85ae64fd9cef39cb8e
Jose Caicedo <jcaiced@gmail.com>
2021-08-10 18:16:03 -0700

    Section 1 Lecture 4 (Video Lecture) Using Microsoft Visual Studio Community 2019 to Write a Simple C++ Program

---
+++ myFirst Program.cpp
@@ -0,0 +1,15 @@
+//Simple C++m Program
+//This program displays a message on the output screen of the program
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Good Morning!" << endl;
+	cout << "Do Homework." << endl;
+	cout << "---------------" << endl;
+
+	return 0;
+
+}
