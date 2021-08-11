9ade898443724cb453275af8fde2e0745bc469f9
Jose Caicedo <jcaiced@gmail.com>
2021-08-10 21:12:53 -0700

    Assignment 2

---
+++ Assignment 2.cpp
@@ -0,0 +1,15 @@
+//Assignment 2 (A Simple C++ Program)
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "+----------------------------+" << endl;
+	cout << "|                            |" << endl;
+	cout << "|        Good Morning!       |" << endl;
+	cout << "|                            |" << endl;
+	cout << "+----------------------------+" << endl;
+
+	system("pause");
+	return 0;
+}
