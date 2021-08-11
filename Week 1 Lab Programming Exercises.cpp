a45eb78a34b80b06c70635a8c5522e793237efca
Jose Caicedo <jcaiced@gmail.com>
2021-08-10 19:25:42 -0700

    Week 1 Lab Programming Exercises

---
+++ Week 1 Lab Programming Exercises.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "+-----------------------------+" << endl;
+	cout << "|   John Smith                |" << endl;
+	cout << "|   Office Assistant          |" << endl;
+	cout << "|   Acounting Office          |" << endl;
+	cout << "|   City of Fresno            |" << endl;
+	cout << "|   (559)- 111-1111           |" << endl;
+	cout << "|   jsmith@cityoffresno.org   |" << endl;
+	cout << "+-----------------------------+" << endl;
+
+	return 0;
+}
