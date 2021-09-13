097b7d9ef910a49cf73153fc7c37478e9d5b121a
Jose Caicedo <jcaiced@gmail.com>
2021-09-12 22:02:54 -0700

    Lecture 2-5 Introduction to While Loop Statement

---
+++ Lecture 2-5 Introduction to While Loop Statement.cpp
@@ -0,0 +1,13 @@
+//CSCI 40 Sample Program 8 (while loop)
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i = 0;
+	while (i < 4) {
+		cout << "Good Morning" << endl;
+		i++;
+	}
+	return 0;
+}
