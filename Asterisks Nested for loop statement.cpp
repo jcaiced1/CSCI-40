d575d63e06e955ef3269fa2b63b985e90b561c9f
Jose Caicedo <jcaiced@gmail.com>
2021-09-29 18:55:08 -0700

    Asterisks Nested for loop statement

---
+++ Asterisks Nested for loop statement.cpp
@@ -0,0 +1,15 @@
+// printing asterisks
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	for (int i = 1; i <= 6; i++) {
+		for (int j = 0; j < i; j++) {
+			cout << "* ";
+		}
+		cout << endl;
+}
+	return 0;
+}
+
