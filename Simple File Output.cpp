4c1f440dd526dc65c5bdac70c748d566eee8643a
Jose Caicedo <jcaiced@gmail.com>
2021-09-30 20:52:00 -0700

    Simple File Output

---
+++ Simple File Output.cpp
@@ -0,0 +1,22 @@
+//Example Simple File Input and Output
+
+#include<iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+	ofstream output;
+
+	//Create a file
+	output.open("numbers.txt");
+
+	//Write numbers
+	output << 95 << " " << 56 << " " << 34;
+
+	//Close file
+	output.close();
+
+	cout << "Done" << endl;
+
+	return 0;
+}
