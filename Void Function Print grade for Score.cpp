b5b4c67935c8c8e174ddc6cecbdfc8e6e69a4fa6
Jose Caicedo <jcaiced@gmail.com>
2021-10-05 22:06:51 -0700

    Void Function Print grade for Score

---
+++ Void Function Print grade for Score.cpp
@@ -0,0 +1,28 @@
+//Void Function- Print Grade for the score
+
+#include<iostream>
+using namespace std;
+
+//Print grade for the socre
+void printGrade(double score)
+{
+	if (score >= 90.0)
+		cout << 'A' << endl;
+	else if (score >= 80.0)
+		cout << 'B' << endl;
+	else if (score >= 70.0)
+		cout << 'C' << endl;
+	else if (score >= 60.0)
+		cout << 'D' << endl;
+	else
+		cout << 'F' << endl;
+}
+
+int main() {
+	cout << "Enter a score: ";
+	double score;
+	cin >> score;
+	cout << "The grade is ";
+	printGrade(score);
+	return 0;
+}
